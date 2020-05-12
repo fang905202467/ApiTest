bb7159822fae17c0390aef7b7c21ab00da7a9968
方坤 <fangkun@gouuse.cn>
2020-05-12 12:36:27 +0800

    Initial commit

---
+++ cases/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Time : 2020/5/12 12:32 
+# @Author : fangkun
+# @File : __init__.py.py 
+# @Software: PyCharm
---
+++ configs/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Time : 2020/5/12 12:33 
+# @Author : fangkun
+# @File : __init__.py.py 
+# @Software: PyCharm
